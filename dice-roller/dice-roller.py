c7c9f3233229cc67d7fd87893168d3a83b8c7737
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-10-13 17:23:10 +0300

    WIP BUG: scrolling

--- dice-roller/dice-roller.py
+++ dice-roller/dice-roller.py
@@ -1,8 +1,9 @@
-import kivy
+import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import NumericProperty
+from kivy.core.window import Window
+Window.size = (400, 400)
 
 
 class MainScreen(ScreenManager):
@@ -11,13 +12,28 @@ class MainScreen(ScreenManager):
 
 
 class FirstScreen(Screen):
-    #some methods
     pass
 
 
 class SecondScreen(Screen):
-    #some methods
-    pass
+    def roll(self, sides):
+        roll_times = self.ids.roll_times.text
+        if not roll_times:
+            roll_times = 1
+        else:
+            roll_times = int(roll_times)
+        modifier = self.ids.modifier.text
+        if not modifier:
+            modifier = 0
+        else:
+            modifier = int(modifier)
+        rolls = []
+        for i in range(roll_times):
+            roll = random.randrange(1, sides+1)
+            rolls.append(roll)
+        total = sum(rolls, modifier)
+        text = f"rolling {roll_times}d{sides}+{modifier}: {rolls} = {total}\n"
+        self.ids.output.text += text
 
 
 class MyKivyApp(App):
